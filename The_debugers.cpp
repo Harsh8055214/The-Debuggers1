00e3ae0ac11c87e37b9798be1d206fc18a9a5776
KODAVALA HARSHKUMAR <202401121@daiict.ac.in>
2025-04-15 22:09:17 +0530

    Update The_debugers.cpp

--- The_debugers.cpp
+++ The_debugers.cpp
@@ -1,134 +1,188 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+int highScore = 0;
 
 void gotoxy(int x, int y) {
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
+    COORD coord = { (SHORT)x, (SHORT)y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
 void hideCursor() {
     HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO cursorInfo;
-    GetConsoleCursorInfo(console, &cursorInfo);
-    cursorInfo.bVisible = false;
-    SetConsoleCursorInfo(console, &cursorInfo);
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = FALSE;
+    SetConsoleCursorInfo(console, &info);
 }
 
-void drawBoard( int paddleX, int ballX, int ballY, int score, int lives){
+void drawBorders() {
+    for (int i = 0; i < 42; i++) {
+        gotoxy(i, 0); cout << "#";
+        gotoxy(i, 24); cout << "#";
+    }
+    for (int i = 0; i < 25; i++) {
+        gotoxy(0, i); cout << "#";
+        gotoxy(41, i); cout << "#";
+    }
+}
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void drawBoard(int paddleX, int paddleSize, int ballX, int ballY, int score, int lives, bool powerUpActive, int powerUpX, int powerUpY) {
     hideCursor();
-	system("cls");
+     setColor(8);
+    system("cls");
+
+    drawBorders();
 
     gotoxy(paddleX, 20);
-    cout << "======";
+    for (int i = 0; i < paddleSize; i++) cout << "=";
 
-   
     gotoxy(ballX, ballY);
     cout << "O";
 
-  
-      for (int i = 0; i < 42; i++) {
-        gotoxy(i, 0);
-        cout << "#";
-        gotoxy(i, 24);
-        cout << "#";
+    if (powerUpActive) {
+        gotoxy(powerUpX, powerUpY);
+        cout << "+";
     }
 
-   for (int i = 0; i < 25; i++) {
-        gotoxy(0, i);
-        cout << "#";
-        gotoxy(41, i);
-        cout << "#";
-    }
-
-
-  
+    setColor(13);
     gotoxy(45, 5);
     cout << "Score: " << score;
-    gotoxy(45, 7);
+    gotoxy(45, 6);
+    cout << "High:  " << highScore;
+    gotoxy(45, 8);
     cout << "Lives: " << lives;
-
-
 }
 
-int main(){
-	system("mode con: lines=30 cols=60");
+void loadHighScore() {
+    ifstream file("highscore.txt");
+    if (file.is_open()) file >> highScore;
+    file.close();
+}
 
-	int paddleX = 20;
-    int ballX = 32;
-    int ballY = 19;
-    int ballDirX = 1;
-    int ballDirY = -1;
+void saveHighScore(int score) {
+    if (score > highScore) {
+        ofstream file("highscore.txt");
+        file << score;
+        file.close();
+    }
+}
+
+void showStartScreen() {
+    system("cls");
+     setColor(10);
+    cout << R"(
+     ____        _     _     _
+    |  _ \ _   | |_ | |__ () __
+    | |) | | | | ' \| '_ \| | '_ \
+    |  _/| || | |) | |) | | | | |
+    ||    \,|./|./||| |_|
+
+)";
+    setColor(12);
+    cout << "Simple Console Pong Game\n";
+    cout << "------------------------\n";
+    cout << "Instructions:\n";
+    cout << "-> Use LEFT and RIGHT arrows to move the paddle\n";
+    cout << "-> Press 'P' to pause\n";
+    cout << "-> Press ESC to exit\n\n";
+    cout << "Press any key to start...";
+    _getch();
+}
+
+int main() {
+    system("mode con: cols=70 lines=30");
+    srand(time(0));
+    loadHighScore();
+    showStartScreen();
+
+    int paddleX = 20, paddleSize = 6;
+    int ballX = 32, ballY = 19;
+    int ballDirX = 1, ballDirY = -1;
     int score = 0;
     int lives = 3;
+    int speed = 100;
 
-     cout<<"Instructions: "<<endl;
-     cout<<"*"<<endl;
-    cout<<"1.Press right and left arrow keys for move paddle."<<endl;
-    cout<<"2.Press Esc key for quit game."<<endl;
+    bool powerUpActive = false;
+    int powerUpX = 0, powerUpY = 0;
 
-    system("pause");
-
-    while(lives > 0){
-    if(_kbhit()){
-    	char key = _getch();
-
-    if (key == 75 && paddleX > 1){
-                paddleX--;
-	     }
-
-	else if (key == 77 && paddleX < 34){
-             paddleX++;
-		}
-
-	else if (key == 27){
-                break;
-		}
-	}
-
-		ballX += ballDirX;
-        ballY += ballDirY;
-
-
-        if (ballY == 20 && ballX >= paddleX && ballX <= paddleX + 5) {
-           ballDirY = -ballDirY;
-            score++;
+    while (lives > 0) {
+        if (_kbhit()) {
+            char key = _getch();
+            if (key == 75 && paddleX > 1) paddleX--;
+            if (key == 77 && paddleX + paddleSize < 40) paddleX++;
+            if (key == 27) break;
+            if (key == 'p' || key == 'P') {
+                gotoxy(20, 12);
+                cout << "Paused. Press any key...";
+                _getch();
+            }
         }
 
-	
-        if (ballX == 1 || ballX == 40)
-            ballDirX = -ballDirX;
+        ballX += ballDirX;
+        ballY += ballDirY;
 
-        if (ballY == 1)
+        if (ballY == 20 && ballX >= paddleX && ballX < paddleX + paddleSize) {
             ballDirY = -ballDirY;
+            score++;
+            if (score % 5 == 0 && speed > 30) speed -= 5;
+            if (score % 10 == 0 && paddleSize > 3) paddleSize--; 
+        }
+
+        if (ballX == 1 || ballX == 40) ballDirX = -ballDirX;
+        if (ballY == 1) ballDirY = -ballDirY;
 
         if (ballY == 23) {
             lives--;
-            ballX = 20;
-            ballY = 19;
-            ballDirY = -ballDirY;
+            ballX = 20; ballY = 19;
+            ballDirY = -1;
         }
 
-		 drawBoard(paddleX, ballX, ballY, score, lives);
-    	Sleep(100);
-	}
+        
+        if (!powerUpActive && rand() % 100 == 0) {
+            powerUpActive = true;
+            powerUpX = 2 + rand() % 37;
+            powerUpY = 1;
+        }
 
-	system("cls");
+        if (powerUpActive) {
+            powerUpY++;
+            if (powerUpY == 20 && powerUpX >= paddleX && powerUpX < paddleX + paddleSize) {
+                lives++;
+                powerUpActive = false;
+            } else if (powerUpY >= 23) {
+                powerUpActive = false;
+            }
+        }
 
-	gotoxy(15, 8);
-	cout<<"Game Over!"<<endl;
+        drawBoard(paddleX, paddleSize, ballX, ballY, score, lives, powerUpActive, powerUpX, powerUpY);
+        Sleep(speed);
+    }
 
-	gotoxy(15, 9);
-    cout << "Final Score: " << score << endl;
+    system("cls");
+    gotoxy(20, 10); cout << "Game Over!";
+    gotoxy(20, 12); cout << "Final Score: " << score;
 
-	gotoxy(15, 10);
+    saveHighScore(score);
 
-    cout << "Press any key to exit..." << endl;
-    _getch();
+    gotoxy(20, 14);
+    cout << "Press R to restart or ESC to quit...";
+    while (true) {
+        char ch = _getch();
+        if (ch == 27) break;
+        if (ch == 'r' || ch == 'R') {
+            main(); 
+            break;
+        }
+    }
 
     return 0;
-
 }
